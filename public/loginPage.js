b43174b3b0ceab4bcfc493dcf0be6ca4d1932c13
Елена Бархатова <hell_sun.net@inbox.ru>
2023-06-06 19:02:56 +0300

    ввод логина и пароля, регистрация нового пользователя обновлен с учетом замечаний

--- public/loginPage.js
+++ public/loginPage.js
@@ -1,14 +1,4 @@
 const userForm = new UserForm();
-userForm.loginFormCallback = function(data) {
-  ApiConnector.login(data, function(response) {
-    console.log(response);
-    if (response.success) {
-      location.reload(); 
-    } else {
-      userForm.setLoginErrorMessage(response.error); 
-    }
-  });
-};
 
 userForm.registerFormCallback = function(data) {
     ApiConnector.register(data, function(response) {
@@ -19,4 +9,14 @@ userForm.registerFormCallback = function(data) {
           userForm.setRegisterErrorMessage(response.error); 
         }
       });
+};
+
+userForm.registerFormCallback = function(data) {
+  ApiConnector.register(data, (response) => {
+    if (response.success) {
+      location.reload();
+    } else {
+      userForm.setRegisterErrorMessage(response.error);
+    }
+  });
 };
