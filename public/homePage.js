48ef77cfbd51d499fd50988a20686fea4d468a7f
Елена Бархатова <hell_sun.net@inbox.ru>
2023-06-08 12:13:27 +0300

    Реализованы операции с деньгами

--- public/homePage.js
+++ public/homePage.js
@@ -26,3 +26,44 @@ function fetchRates() {
 
 fetchRates();
 setInterval(() => fetchRates(), 60000);
+
+const moneyManager = new MoneyManager();
+moneyManager.addMoneyCallback = (data) => {
+  ApiConnector.addMoney(data, (response) => {
+    if (response.success) {
+      ProfileWidget.showProfile(response.data);
+      const { amount, currency } = data;
+      const message = `Баланс пополнен на ${amount} ${currency}.`;
+      moneyManager.setMessage(true, message);
+    } else {
+      moneyManager.setMessage(false, response.error);
+    };
+  });
+};
+
+moneyManager.conversionMoneyCallback = (data) => {
+  ApiConnector.convertMoney(data, (response) => {
+    if (response.success) {
+      ProfileWidget.showProfile(response.data);
+      const { fromCurrency, targetCurrency } = data;
+      const message = `Конвертация из ${fromCurrency} в ${targetCurrency} выполнена успешно.`;
+      moneyManager.setMessage(true, message);
+    } else {
+      moneyManager.setMessage(false, response.error);
+    };
+  });
+};
+
+moneyManager.sendMoneyCallback = (data) => {
+  ApiConnector.transferMoney(data, (response) => {
+    if (response.success) {
+      ProfileWidget.showProfile(response.data);
+      const { userName, amount, currency } = data;
+      const message = `Перевод на сумму ${amount} ${currency} выполнен для пользователя ${userName}.`;
+      moneyManager.setMessage(true, message);
+    } else {
+      moneyManager.setMessage(false, response.error);
+    };
+  });
+};
+
